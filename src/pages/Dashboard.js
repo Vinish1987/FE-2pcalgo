713b47f6b3ebd900ed88e8071a54c9c9776d5a85
Vinish1987 <vini12345@gmail.com>
2025-04-11 14:16:41 +0530

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,30 +1,16 @@
 // src/pages/Dashboard.js
-import React, { useState } from 'react';
-import Header from '../components/Header';
+
+import React from 'react';
 import Chart from '../components/Chart';
-import TrapZones from '../components/TrapZones';
 
 const Dashboard = () => {
-  const [lotSize, setLotSize] = useState(1);
-  const [balance, setBalance] = useState(20000);
-
-  const handleConnect = () => {
-    alert("Connecting to Zerodha...");
-  };
-
   return (
-    <div style={{ background: 'black', minHeight: '100vh', color: 'white' }}>
-      <Header 
-        balance={balance}
-        lotSize={lotSize}
-        setLotSize={setLotSize}
-        onConnect={handleConnect}
-      />
+    <div>
+      {/* Other components like Header */}
       <Chart />
-      <TrapZones />
+      {/* Other components like TrapZones */}
     </div>
   );
 };
 
 export default Dashboard;
-
