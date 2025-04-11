fd4803e81078c0eda4d2e032db7f420ccc2d8d9c
Vinish1987 <vini12345@gmail.com>
2025-04-11 14:11:37 +0530

    Create ChartSection.js

---
+++ src/components/ChartSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ChartSection = () => {
+  return (
+    <div style={{ padding: '10px' }}>
+      <div style={{ backgroundColor: '#111', height: '500px', borderRadius: '10px' }}>
+        {/* Your chart will be embedded here later */}
+        <p style={{ color: '#888', textAlign: 'center', paddingTop: '200px' }}>Chart Loading...</p>
+      </div>
+    </div>
+  );
+};
+
+export default ChartSection;
