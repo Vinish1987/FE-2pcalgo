37b57b421d11fdf9c1f4e18912b9c83ab2559abb
Vinish1987 <vini12345@gmail.com>
2025-04-11 14:07:31 +0530

    Update TrapZones.js

--- src/components/TrapZones.js
+++ src/components/TrapZones.js
@@ -1,1 +1,15 @@
+// src/components/TrapZones.js
+import React from 'react';
+
+const TrapZones = () => {
+  return (
+    <div style={{ margin: '1rem', borderRadius: '10px', overflow: 'hidden' }}>
+      <div style={{ background: '#ffcccc', height: '80px' }}></div>
+      <div style={{ background: '#cce6ff', height: '80px' }}></div>
+      <div style={{ background: '#b2f2bb', height: '80px' }}></div>
+    </div>
+  );
+};
+
+export default TrapZones;
 
